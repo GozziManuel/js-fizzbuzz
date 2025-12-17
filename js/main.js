4f26d6ce813fe62dc2b5a784281dac047827415b
Manuel Gozzi <manuel2006gozzi@gmail.com>
2025-12-17 18:22:41 +0100

    added color and fixed buzz

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 // Scrivo la formula che genera i numeri 1-100
 let numStart = 0;
 let numEnd = 100;
-let fizz = "fizz";
-let buzz = "buzz";
-let fizzbuzz = fizz + buzz;
+const styleFizz = "background-color: red; color: black; font-size: 20px";
+const styleFizzBuzz = "background-color: green; color: black; font-size: 20px";
+const styleBuzz = "background-color: blue; color: black; font-size: 20px";
+
 for (let i = numStart; i <= numEnd; i++) {
   //     SE multipli di entrambi scrivere fizzbuzz "fizz + buzz"
   if (i % 3 == 0 && i % 5 == 0) {
-    console.log(fizzbuzz);
+    console.log("%cFizzBuzz", styleFizzBuzz);
     //     SE multipli di 5 scrivere buzz
   } else if (i % 5 == 0) {
-    console.log(buzz);
+    console.log("%cBuzz", styleBuzz);
     //     SE multipli di 3 scrivere al posto del numero fizz
   } else if (i % 3 == 0) {
-    console.log(buzz);
+    console.log("%cFizz", styleFizz);
   } else {
     console.log(i);
   }
