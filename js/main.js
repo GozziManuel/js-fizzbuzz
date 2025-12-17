1c4cf53fb40f9ead7ce95f6303ee3b7f3b781450
Manuel Gozzi <manuel2006gozzi@gmail.com>
2025-12-17 13:20:17 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Scrivo la formula che genera i numeri 1-100
+
+//     SE multipli di 3 scrivere al posto del numero fizz
+//     SE multipli di 5 scrivere buzz
+//     SE multipli di entrambi scrivere fizzbuzz "fizz + buzz"
