364fc7f6cff82dda922d3b24cc4cd1968a57152d
Manuel Gozzi <manuel2006gozzi@gmail.com>
2025-12-17 16:22:18 +0100

    multipli di 3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 // Scrivo la formula che genera i numeri 1-100
-for (let i = 0; i <= 100; i++) {
-  console.log(i);
+let numStart = 0;
+let numEnd = 100;
+for (let i = numStart; i <= numEnd; i++) {
+  //     SE multipli di 3 scrivere al posto del numero fizz
+  if (i % 3 == 0) {
+    console.log("fizz");
+  } else {
+    console.log(i);
+  }
 }
-//     SE multipli di 3 scrivere al posto del numero fizz
+
 //     SE multipli di 5 scrivere buzz
 //     SE multipli di entrambi scrivere fizzbuzz "fizz + buzz"
