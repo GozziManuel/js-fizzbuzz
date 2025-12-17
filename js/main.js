9648470194273d1bf2030a5b1d1222e6b74e5a04
Manuel Gozzi <manuel2006gozzi@gmail.com>
2025-12-17 16:34:57 +0100

    fixed order + other 2 mult

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 // Scrivo la formula che genera i numeri 1-100
 let numStart = 0;
 let numEnd = 100;
+let fizz = "fizz";
+let buzz = "buzz";
+let fizzbuzz = fizz + buzz;
 for (let i = numStart; i <= numEnd; i++) {
-  //     SE multipli di 3 scrivere al posto del numero fizz
-  if (i % 3 == 0) {
-    console.log("fizz");
+  //     SE multipli di entrambi scrivere fizzbuzz "fizz + buzz"
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log(fizzbuzz);
+    //     SE multipli di 5 scrivere buzz
+  } else if (i % 5 == 0) {
+    console.log(buzz);
+    //     SE multipli di 3 scrivere al posto del numero fizz
+  } else if (i % 3 == 0) {
+    console.log(buzz);
   } else {
     console.log(i);
   }
 }
-
-//     SE multipli di 5 scrivere buzz
-//     SE multipli di entrambi scrivere fizzbuzz "fizz + buzz"
